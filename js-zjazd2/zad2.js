f50ec2575c6069ab99d3951e1f6107aabc5b64bf
Daniel Chmur <daniel.chmur@gmail.com>
2021-11-02 10:48:07 +0100

    ttt

--- js-zjazd2/zad2.js
+++ js-zjazd2/zad2.js
@@ -3,7 +3,7 @@
 arr = [1, 6, 23, 8, 4, 8, 3, 7]
 
 function sumOfFirstAndLastElementOfAnArray(array) {
-  return array[array.length - 1]
+  return array[0] + array[array.length - 1]
 }
 
 console.log(sumOfFirstAndLastElementOfAnArray(arr))
