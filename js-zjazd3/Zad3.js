41395edf58351d746117bb32888c73db18970769
Daniel Chmur <daniel.chmur@gmail.com>
2021-11-05 18:05:51 +0100

    zjazd 3 zad3

--- js-zjazd3/Zad3.js
+++ js-zjazd3/Zad3.js
@@ -1,4 +1,4 @@
-// 3) Create an array(by hand) of objects and call sum() function in context of each one of them. Sum function is to come from this object BaseObject= {x,y, sum: function (){ returnthis.x+this.y}}
+// 3) Create an array(by hand) of objects and call sum() function in context of each one of them. Sum function is to come from this object BaseObject= {x,y, sum: function () {returnthis.x+this.y}}
 
 let array = [
   { x: 2, y: 6 },
