73c2940517028e9b74dd06feed1979b607dbfb66
Daniel Chmur <daniel.chmur@gmail.com>
2021-11-06 18:23:23 +0100

    zjazd3 zad3

--- js-zjazd3/Zad3.js
+++ js-zjazd3/Zad3.js
@@ -1,13 +1,11 @@
 // 3) Create an array(by hand) of objects and call sum() function in context of each one of them. Sum function is to come from this object BaseObject = {x, y, sum: function () { return this.x + this.y }}
 
+let x, y
 let array = [
   { x: 2, y: 6 },
   { x: -3, y: 7 },
   { x: 5, y: -5 },
 ]
-
-let x, y
-let [obj1, obj2, obj3] = array
 
 BaseObject = {
   x,
@@ -17,6 +15,17 @@ BaseObject = {
   },
 }
 
-console.log(BaseObject.sum.call(obj1))
-console.log(BaseObject.sum.call(obj2))
-console.log(BaseObject.sum.call(obj3))
+let getOneObject = function (array, index) {
+  let { x, y } = array[index]
+  return {
+    x,
+    y,
+  }
+}
+
+let showSum = (function (array) {
+  for (let i = 0; i < array.length; i++) {
+    let obj = getOneObject(array, i)
+    console.log(BaseObject.sum.call(obj))
+  }
+})(array)
