bd859983f34b9dcf7be56febce1a27cfea4d5265
Daniel Chmur <daniel.chmur@gmail.com>
2021-11-08 08:05:30 +0100

    zad4 zjazd3

--- js-zjazd3/Zad1.js
+++ js-zjazd3/Zad1.js
@@ -1,29 +1,29 @@
 //1) Create an iffe that returns an object with fields: function setValue(), function showValue() and function reverseValue()Calling functions either logs the value or reverse it in an object. If value was not provided yet or is empty showValue function is to return information about that. Value can be type string or number. reverseValue(): If number do (*(-1)), if string reverse it. Closure pattern.
 
 let result = (value) =>
-  (function (copiedValue) {
-    copiedValue = value
+	(function (copiedValue) {
+		copiedValue = value
 
-    let setValue = (function () {
-      return copiedValue
-    })()
+		let setValue = (function () {
+			return copiedValue
+		})()
 
-    let showValue = (function () {
-      return copiedValue ? copiedValue : "Nie podano wartości"
-    })()
+		let showValue = (function () {
+			return copiedValue ? copiedValue : "Nie podano wartości"
+		})()
 
-    let reverseValue = (function () {
-      return typeof copiedValue === "string"
-        ? copiedValue.split("").reverse().join("")
-        : copiedValue * -1
-    })()
+		let reverseValue = (function () {
+			return typeof copiedValue === "string"
+				? copiedValue.split("").reverse().join("")
+				: copiedValue * -1
+		})()
 
-    return {
-      setValue,
-      showValue,
-      reverseValue,
-    }
-  })(value)
+		return {
+			setValue,
+			showValue,
+			reverseValue,
+		}
+	})(value)
 
 console.log("null", result())
 console.log(500, result(500))
