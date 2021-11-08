b28781df56daf5208a6c72badd65b0ff3fd4a708
Daniel Chmur <daniel.chmur@gmail.com>
2021-11-08 06:37:00 +0100

    zjazd3 zad4

---
+++ js-zjazd3/Zad4.js
@@ -0,0 +1,33 @@
+// Given an array of objects, for each object. Call operation() function in context of next object. If object is last, got back to start of the array for operation function. In loop;
+
+const givenArray = [
+  {
+    x: 1,
+    y: "object one value",
+    operation: function () {
+      return "object one prefix" + this.x + this.y;
+    },
+  },
+  {
+    x: 2,
+    y: "object two value",
+    operation: function () {
+      return "object two prefix" + this.x + this.y;
+    },
+  },
+  {
+    x: 3,
+    y: "object three value",
+    operation: function () {
+      return "object three prefix" + this.x + this.y;
+    },
+  },
+];
+
+for (let i = 0; i < givenArray.length; i++) {
+  if (i === givenArray.length - 1) {
+    console.log(givenArray[0].operation.call(givenArray[0]));
+  } else {
+    console.log(givenArray[i + 1].operation.call(givenArray[i + 1]));
+  }
+}
