69d05619a79aa393ef6a4ec3c2d47529884f7a97
Daniel Chmur <daniel.chmur@gmail.com>
2021-11-12 16:42:16 +0100

    z3z3

--- js-zjazd4/Zad3.js
+++ js-zjazd4/Zad3.js
@@ -1,140 +1,166 @@
 let fs = require("fs");
-let path = "C:/Users/dchmur/Desktop/jsHomework/js-zjazd4/Data.json";
+let path = "C:/Users/LENOVO/Desktop/jsHomework/js-zjazd4/Data.json";
 
 //wczytuje dane z pliku
 let loadedData = (function loadData(path) {
-	return JSON.parse(fs.readFileSync(path, "utf-8"));
+  return JSON.parse(fs.readFileSync(path, "utf-8"));
 })(path);
 
 // utworzenie drugiej struktury danych
 function Transaction(index, cost, detailsOfPayent) {
-	let { Type, company, date } = detailsOfPayent;
-	this.index = index;
-	this.cost = cost;
-	this.type = Type;
-	this.company =
-		company.split("")[0] +
-		company.toLowerCase().split("").slice(1).join("");
-	this.date = date;
-	this.dateYear = (function (date) {
-		let currentYear = date.split("-")[2];
-		return parseInt(currentYear);
-	})(date);
-	this.dateMonth = (function (date) {
-		let currentYear = date.split("-")[1];
-		return parseInt(currentYear);
-	})(date);
-	this.dateDayOfTheWeek = (function (date) {
-		let datecurrentDate = date.split("-");
-		let day = datecurrentDate[0];
-		let month = datecurrentDate[1];
-		let year = datecurrentDate[2];
-		let dateResult = new Date(year, month - 1, day);
-		// 0 is sunday
-		return dateResult.getDay();
-	})(date);
+  let { Type, company, date } = detailsOfPayent;
+  this.index = index;
+  this.cost = cost;
+  this.type = Type;
+  this.company =
+    company.split("")[0] + company.toLowerCase().split("").slice(1).join("");
+  this.date = date;
+  this.dateYear = (function (date) {
+    let currentYear = date.split("-")[2];
+    return parseInt(currentYear);
+  })(date);
+  this.dateMonth = (function (date) {
+    let currentYear = date.split("-")[1];
+    return parseInt(currentYear);
+  })(date);
+  this.dateDayOfTheWeek = (function (date) {
+    let datecurrentDate = date.split("-");
+    let day = datecurrentDate[0];
+    let month = datecurrentDate[1];
+    let year = datecurrentDate[2];
+    let dateResult = new Date(year, month - 1, day);
+    // 0 is Sunday
+    return dateResult.getDay();
+  })(date);
 }
 
 let data = loadedData.map((t) => {
-	return new Transaction(
-		t.index,
-		t.cost,
-		t.detailsOfPayent,
-		t.company,
-		t.type,
-		t.dateYear,
-		t.dateMonth,
-		t.dateDayOfTheWeek
-	);
+  return new Transaction(
+    t.index,
+    t.cost,
+    t.detailsOfPayent,
+    t.company,
+    t.type,
+    t.dateYear,
+    t.dateMonth,
+    t.dateDayOfTheWeek
+  );
 });
 
 let resultObject = (function (data) {
-	innerObject = {
-		spentIn2014: spentIn2014(),
-		companiesEarns: companiesEarns(),
-		mostSpendingType: mostSpendingType(),
-		spendingsInMonth: spendingsInMonth(),
-	};
+  innerObject = {
+    spentIn2014: spentIn2014(),
+    companiesEarns: companiesEarns(),
+    mostSpendingType: mostSpendingType(),
+    spendingsInMonth: spendingsInMonth(),
+    spendingsInEachDayofTheWeek: spendingsInEachDayofTheWeek(),
+  };
 
-	function spentIn2014() {
-		let sumOfCost = 0;
-		data.forEach((item) => {
-			if (item.dateYear === 2014) {
-				sumOfCost += Number(item.cost);
-			}
-		});
-		sumOfCost = sumOfCost.toFixed(2);
-		return sumOfCost;
-	}
+  function spentIn2014() {
+    let sumOfCost = 0;
+    data.forEach((item) => {
+      if (item.dateYear === 2014) {
+        sumOfCost += Number(item.cost);
+      }
+    });
+    sumOfCost = sumOfCost.toFixed(2);
+    return sumOfCost;
+  }
 
-	function companiesEarns() {
-		let companies = [...new Set(data.map((item) => item.company))];
-		let earnings = [];
+  function companiesEarns() {
+    let companies = [...new Set(data.map((item) => item.company))];
+    let earnings = [];
 
-		let companyEarnings = 0;
-		companies.forEach((company) => {
-			data.forEach((item) => {
-				if (item.company === company) {
-					companyEarnings += Number(item.cost);
-				}
-			});
-			earnings.push(companyEarnings.toFixed(2));
-		});
+    let companyEarnings = 0;
+    companies.forEach((company) => {
+      data.forEach((item) => {
+        if (item.company === company) {
+          companyEarnings += Number(item.cost);
+        }
+      });
+      earnings.push(companyEarnings.toFixed(2));
+    });
 
-		//tworzę obiekt z macierzy companies i earnings
-		let result = {};
-		companies.forEach(function (key, i) {
-			result[key] = earnings[i];
-		});
-		return result;
-	}
+    //tworzę obiekt z macierzy companies i earnings
+    let result = {};
+    companies.forEach(function (key, i) {
+      result[key] = earnings[i];
+    });
+    return result;
+  }
 
-	function mostSpendingType() {
-		let types = [...new Set(data.map((item) => item.type))];
-		let spendings = [];
+  function mostSpendingType() {
+    let types = [...new Set(data.map((item) => item.type))];
+    let spendings = [];
 
-		for (let type of types) {
-			let typeSpendings = 0;
-			for (let item of data) {
-				if (item.type === type) {
-					typeSpendings += Number(item.cost);
-				}
-			}
-			spendings.push(typeSpendings.toFixed(2));
-		}
+    for (let type of types) {
+      let typeSpendings = 0;
+      for (let item of data) {
+        if (item.type === type) {
+          typeSpendings += Number(item.cost);
+        }
+      }
+      spendings.push(typeSpendings.toFixed(2));
+    }
 
-		let result = {};
-		types.forEach(function (key, i) {
-			result[key] = spendings[i];
-		});
-		return result;
-	}
+    let result = {};
+    types.forEach(function (key, i) {
+      result[key] = spendings[i];
+    });
+    return result;
+  }
 
-	function spendingsInMonth() {
-		let months = [...new Set(data.map((item) => item.dateMonth))];
-		let spendings = [];
+  function spendingsInMonth() {
+    let months = [...new Set(data.map((item) => item.dateMonth))];
+    let spendings = [];
 
-		for (let month of months) {
-			let MonthSpendings = 0;
-			for (let item of data) {
-				if (item.dateMonth === month) {
-					MonthSpendings += Number(item.cost);
-				}
-			}
-			spendings.push(MonthSpendings.toFixed(2));
-		}
+    for (let month of months) {
+      let monthSpendings = 0;
+      for (let item of data) {
+        if (item.dateMonth === month) {
+          monthSpendings += Number(item.cost);
+        }
+      }
+      spendings.push(monthSpendings.toFixed(2));
+    }
 
-		let result = {};
-		months.forEach(function (key, i) {
-			result[key] = spendings[i];
-		});
-		return result;
-	}
+    let result = {};
+    months.forEach(function (key, i) {
+      result[key] = spendings[i];
+    });
+    return result;
+  }
 
-	return innerObject;
+  function spendingsInEachDayofTheWeek() {
+    let daysOfWeek = [...new Set(data.map((item) => item.dateDayOfTheWeek))];
+    let spendings = [];
+
+    for (let day of daysOfWeek) {
+      let daySpendings = 0;
+      for (let item of data) {
+        if (item.dateDayOfTheWeek === day) {
+          daySpendings += Number(item.cost);
+        }
+      }
+      spendings.push(daySpendings.toFixed(2));
+    }
+
+    let result = {};
+    daysOfWeek.forEach(function (key, i) {
+      result[key] = spendings[i];
+    });
+
+    return result;
+  }
+
+  return innerObject;
 })(data);
 
 console.log("Spendings in 2014:", resultObject.spentIn2014);
 console.log("Companies earnings", resultObject.companiesEarns);
 console.log("Spendings by type:", resultObject.mostSpendingType);
 console.log("Spendings by month:", resultObject.spendingsInMonth);
+console.log(
+  "Spendings by day of the week:",
+  resultObject.spendingsInEachDayofTheWeek
+);
