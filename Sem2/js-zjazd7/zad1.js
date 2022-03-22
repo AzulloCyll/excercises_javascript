288d8790069de8f6d05803aadb093e3120057775
Daniel Chmur <daniel.chmur@gmail.com>
2022-03-22 14:59:14 +0100

    zad2

--- Sem2/js-zjazd7/zad1.js
+++ Sem2/js-zjazd7/zad1.js
@@ -1,16 +1,68 @@
 // Write a program that automatically converts English text to Morse code and vice versa.
 
-let morseCode = {
-  a: ".-",
-  b: "-...",
-  c: "-.-.",
-  d: "-..",
-  e: ".",
-  f: "..-.",
-  g: "--.",
-  h: "....",
-  i: "..",
-  j: ".---",
-  k: "-.-",
-  l: ".-..",
-};
+class Translator {
+  constructor(text, translateMethod) {
+    this.text = text;
+    this.translateMethod = translateMethod;
+  }
+
+  translate = () => {
+    return this.translateMethod(this.text);
+  };
+}
+
+class TranslationEngine {
+  toMorseCode = (text) => {
+    let morseCode = {
+      a: ".-",
+      b: "-...",
+      c: "-.-.",
+      d: "-..",
+      e: ".",
+      f: "..-.",
+      g: "--.",
+      h: "....",
+      i: "..",
+      j: ".---",
+      k: "-.-",
+      l: ".-..",
+      m: "--",
+      n: "-.",
+      o: "---",
+      p: ".--.",
+      q: "--.-",
+      r: ".-.",
+      s: "...",
+      t: "-",
+      u: "..-",
+      v: "...-",
+      w: ".--",
+      x: "-..-",
+      y: "-.--",
+      z: "--..",
+      1: ".----",
+      2: "..---",
+      3: "...--",
+      4: "....-",
+      5: ".....",
+      6: "-....",
+      7: "--...",
+      8: "---..",
+      9: "----.",
+      0: "-----",
+    };
+
+    const decodeLetter = (letter) => morseCode[letter] || letter;
+    const decodeWord = (word) => word.split("").map(decodeLetter).join(" ");
+    const decodeText = (text) => text.split(" ").map(decodeWord).join("  ");
+
+    return decodeText(text.toLowerCase());
+  };
+}
+
+let translationEngine = new TranslationEngine();
+let toMorseCodeTranslator = new Translator(
+  "Pack my box with five dozen liquor jugs",
+  translationEngine.toMorseCode
+);
+console.log(toMorseCodeTranslator.translate());
