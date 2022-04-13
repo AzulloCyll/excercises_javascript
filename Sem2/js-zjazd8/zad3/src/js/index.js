b4e2c918d43c247e0312c5ccf85ac54e1332f56b
Daniel Chmur <daniel.chmur@gmail.com>
2022-04-13 04:07:39 +0200

    if

--- Sem2/js-zjazd8/zad3/src/js/index.js
+++ Sem2/js-zjazd8/zad3/src/js/index.js
@@ -54,10 +54,58 @@ class Game {
 }
 
 function getBall(board) {
-	return ball;
+	let X = board.length;
+	let Y = board[0].length;
+	let ballx, bally;
+	let vectorx, vectory;
+
+	for (let x = 0; x < X; x++) {
+		for (let y = 0; y < Y; y++) {
+			if (board[x][y] === "1") {
+				ballx = x;
+				bally = y;
+
+				console.log(board[ballx + 0][bally + 1]);
+
+				// XXX
+				// X10
+				// X00
+				if (
+					board[ballx + 1][bally + 1] === "0" &&
+					board[ballx][bally + 1] === "0" &&
+					board[ballx + 1][bally] === "0"
+				) {
+					vectorx = 1;
+					vectory = 1;
+				}
+
+				// X00
+				// X10
+				// XXX
+				if (
+					board[ballx - 1][bally + 1] === "0" &&
+					board[ballx - 1][bally] === "0" &&
+					board[ballx][bally + 1] === "0"
+				) {
+					vectorx = 1;
+					vectory = -1;
+				}
+			}
+		}
+	}
+
+	let vector = new Vector(vectorx, vectory);
+
+	let ball = new Ball(ballx, bally, vector);
+
+	console.log(ball);
+
+	return null;
 }
 
-let vector = new Vector(1, 1);
-let ball = new Ball(1, 1, vector);
-let game = new Game(new Ball(1, 1, vector), board);
-game.start();
+getBall(board);
+
+// let vector = new Vector(1, 1);
+// let ball = new Ball(1, 1, vector);
+// let game = new Game(new Ball(1, 1, vector), board);
+// game.start();
